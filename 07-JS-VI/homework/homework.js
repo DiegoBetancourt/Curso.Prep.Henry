41fb90fd01c026d49cc5c70712115add0b673235
Diana Carolina Castaño <diferbet@gmail.com>
2021-06-22 22:03:13 -0500

    finish homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -16,7 +16,7 @@ function invocarCallback(cb) {
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
-  //Tu código:
+  //Tu codigo:
   return cb(n1, n2);
 }
 
