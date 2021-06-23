112640285064a44ef750fdacce96bbf1453e1cd0
Diana Carolina Castaño <diferbet@gmail.com>
2021-06-22 20:21:28 -0500

    finish homework 06

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -35,7 +35,7 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
-  String.prototype.reverse = function () {
+  String.prototype.reverse = function() {
     var stringInvertida = "";
     for( var i = this.length - 1; i>=0; i--) {
       stringInvertida = stringInvertida + this.charAt(i);
